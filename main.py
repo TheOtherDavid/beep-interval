8c424aea5f9791bd9a071ba9be3ff1fc1b34ab8d
David <davidhand1988@gmail.com>
2022-05-10 19:08:52 -0400

    Adding a print statement for when it beeps.

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ import time
 from playsound import playsound
 
 interval = 30
-repeat = 0
+repeat = 4
 
 playsound("beep.mp3")
 
 for x in range(repeat):
     time.sleep(interval)
+    print("Playing beep")
     playsound("beep.mp3")
 
 #Two rapid beeps at the end to let you know the program ended
+time.sleep(.25)
 playsound("beep.mp3")
 time.sleep(.01)
 playsound("beep.mp3")
